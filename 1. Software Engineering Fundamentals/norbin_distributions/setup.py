26c25ae048aa9d611e9d4e37dcd4a24a0587ff70
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2020-05-05 08:27:16 -0300

    update with gaussbin_distribution package

---
+++ 1. Software Engineering Fundamentals/norbin_distributions/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussbin_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distribution',
+      packages=['gaussbin_distributions'],
+      author='Rodrigo Bernardo Medeiros',
+      author_email='rodrimedeiros@gmail.com',
+      zip_safe=False)
